25182f5dd6df6d2c4b9dfb46455089f211e95479
erw7 <erw7.github@gmail.com>
2018-11-22 12:59:45 +0900

    build: BuildLibuv.cmake: use libuv upstream CMakeLists.txt

--- third-party/cmake/BuildLibuv.cmake
+++ third-party/cmake/BuildLibuv.cmake
@@ -1,18 +1,18 @@
 include(CMakeParseArguments)
 
-# BuildLibuv(TARGET targetname CONFIGURE_COMMAND ... BUILD_COMMAND ... INSTALL_COMMAND ...)
+# BuildLibuv(TARGET targetname CONFIGURE_COMMAND ... PATCH_COMMAND ... BUILD_COMMAND ... INSTALL_COMMAND ...)
 # Reusable function to build libuv, wraps ExternalProject_Add.
 # Failing to pass a command argument will result in no command being run
 function(BuildLibuv)
   cmake_parse_arguments(_libuv
-    "BUILD_IN_SOURCE"
-    "TARGET"
-    "CONFIGURE_COMMAND;BUILD_COMMAND;INSTALL_COMMAND"
+    ""
+    ""
+    "PATCH_COMMAND;CONFIGURE_COMMAND;BUILD_COMMAND;INSTALL_COMMAND"
     ${ARGN})
 
-  if(NOT _libuv_CONFIGURE_COMMAND AND NOT _libuv_BUILD_COMMAND
-        AND NOT _libuv_INSTALL_COMMAND)
-    message(FATAL_ERROR "Must pass at least one of CONFIGURE_COMMAND, BUILD_COMMAND, INSTALL_COMMAND")
+  if(NOT _libuv_PATCH_COMMAND AND NOT _libuv_CONFIGURE_COMMAND
+      AND NOT _libuv_BUILD_COMMAND AND NOT _libuv_INSTALL_COMMAND)
+    message(FATAL_ERROR "Must pass at least one of PATCH_COMMAND, CONFIGURE_COMMAND, BUILD_COMMAND, INSTALL_COMMAND")
   endif()
   if(NOT _libuv_TARGET)
     set(_libuv_TARGET "libuv")
@@ -30,58 +30,60 @@ function(BuildLibuv)
       -DTARGET=${_libuv_TARGET}
       -DUSE_EXISTING_SRC_DIR=${USE_EXISTING_SRC_DIR}
       -P ${CMAKE_CURRENT_SOURCE_DIR}/cmake/DownloadAndExtractFile.cmake
-    BUILD_IN_SOURCE ${_libuv_BUILD_IN_SOURCE}
+    PATCH_COMMAND "${_libuv_PATCH_COMMAND}"
     CONFIGURE_COMMAND "${_libuv_CONFIGURE_COMMAND}"
     BUILD_COMMAND "${_libuv_BUILD_COMMAND}"
     INSTALL_COMMAND "${_libuv_INSTALL_COMMAND}")
 endfunction()
 
-set(UNIX_CFGCMD sh ${DEPS_BUILD_DIR}/src/libuv/autogen.sh &&
-  ${DEPS_BUILD_DIR}/src/libuv/configure --with-pic --disable-shared
-  --prefix=${DEPS_INSTALL_DIR} --libdir=${DEPS_INSTALL_DIR}/lib
-  CC=${DEPS_C_COMPILER})
-
-if(UNIX)
-  BuildLibuv(
-    CONFIGURE_COMMAND ${UNIX_CFGCMD} MAKE=${MAKE_PRG}
-    INSTALL_COMMAND ${MAKE_PRG} V=1 install)
-
-elseif(MINGW AND CMAKE_CROSSCOMPILING)
-  # Build libuv for the host
-  BuildLibuv(TARGET libuv_host
-    CONFIGURE_COMMAND sh ${DEPS_BUILD_DIR}/src/libuv_host/autogen.sh && ${DEPS_BUILD_DIR}/src/libuv_host/configure --with-pic --disable-shared --prefix=${HOSTDEPS_INSTALL_DIR} CC=${HOST_C_COMPILER}
-    INSTALL_COMMAND ${MAKE_PRG} V=1 install)
-
-  # Build libuv for the target
-  BuildLibuv(
-    CONFIGURE_COMMAND ${UNIX_CFGCMD} --host=${CROSS_TARGET}
-    INSTALL_COMMAND ${MAKE_PRG} V=1 install)
-
-elseif(WIN32)
-
-  set(UV_OUTPUT_DIR ${DEPS_BUILD_DIR}/src/libuv/${CMAKE_BUILD_TYPE})
+if(WIN32)
   if(MSVC)
-    set(BUILD_SHARED ON)
-  elseif(MINGW)
     set(BUILD_SHARED OFF)
   else()
-    message(FATAL_ERROR "Trying to build libuv in an unsupported system ${CMAKE_SYSTEM_NAME}/${CMAKE_C_COMPILER_ID}")
+    set(BUILD_SHARED ON)
   endif()
-  BuildLibUv(BUILD_IN_SOURCE
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy
-        ${CMAKE_CURRENT_SOURCE_DIR}/cmake/LibuvCMakeLists.txt
-        ${DEPS_BUILD_DIR}/src/libuv/CMakeLists.txt
-      COMMAND ${CMAKE_COMMAND} ${DEPS_BUILD_DIR}/src/libuv/CMakeLists.txt
-        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-        -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
-        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -DBUILD_SHARED_LIBS=${BUILD_SHARED}
-        -DCMAKE_INSTALL_PREFIX=${DEPS_INSTALL_DIR}
-    BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE}
-    INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install --config ${CMAKE_BUILD_TYPE})
-
+  set(LIBUV_PATCH_COMMAND ${GIT_EXECUTABLE} -C ${DEPS_BUILD_DIR}/src/libuv init
+    COMMAND ${GIT_EXECUTABLE} -C ${DEPS_BUILD_DIR}/src/libuv apply --ignore-whitespace
+    ${CMAKE_CURRENT_SOURCE_DIR}/patches/libuv-Improve-CMakeLists.patch)
 else()
-  message(FATAL_ERROR "Trying to build libuv in an unsupported system ${CMAKE_SYSTEM_NAME}/${CMAKE_C_COMPILER_ID}")
+  set(BUILD_SHARED OFF)
+  set(LIBUV_PATCH_COMMAND patch -d ${DEPS_BUILD_DIR}/src/libuv -Np1 --input
+    ${CMAKE_CURRENT_SOURCE_DIR}/patches/libuv-Improve-CMakeLists.patch)
 endif()
+
+set(LIBUV_CONFIGURE_COMMAND ${CMAKE_COMMAND} ${DEPS_BUILD_DIR}/src/libuv
+  -DCMAKE_INSTALL_PREFIX=${DEPS_INSTALL_DIR}
+  -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+  -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
+  -DBUILD_SHARED_LIBS=${BUILD_SHARED})
+
+if(UNIX)
+  list(APPEND LIBUV_CONFIGURE_COMMAND
+    "-DCMAKE_C_FLAGS:STRING=${CMAKE_C_COMPILER_ARG1} -fPIC")
+  list(APPEND LIBUV_CONFIGURE_COMMAND
+    -DCMAKE_INSTALL_LIBDIR=${DEPS_INSTALL_DIR}/lib)
+endif()
+
+set(LIBUV_BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE})
+set(LIBUV_INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install --config ${CMAKE_BUILD_TYPE})
+
+if(MINGW AND CMAKE_CROSSCOMPILING)
+  get_filename_component(TOOLCHAIN ${CMAKE_TOOLCHAIN_FILE} REALPATH)
+  set(LIBUV_CONFIGURE_COMMAND ${CMAKE_COMMAND} ${DEPS_BUILD_DIR}/src/libuv
+    -DCMAKE_INSTALL_PREFIX=${DEPS_INSTALL_DIR}
+    # Pass toolchain
+    -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN}
+    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+    # Hack to avoid -rdynamic in Mingw
+    -DCMAKE_SHARED_LIBRARY_LINK_C_FLAGS="")
+  set(LIBUV_PATCH_COMMAND patch -d ${DEPS_BUILD_DIR}/src/libuv -Np1 --input
+    ${CMAKE_CURRENT_SOURCE_DIR}/patches/libuv-Improve-CMakeLists.patch)
+endif()
+
+BuildLibuv(PATCH_COMMAND ${LIBUV_PATCH_COMMAND}
+  CONFIGURE_COMMAND ${LIBUV_CONFIGURE_COMMAND}
+  BUILD_COMMAND ${LIBUV_BUILD_COMMAND}
+  INSTALL_COMMAND ${LIBUV_INSTALL_COMMAND})
 
 list(APPEND THIRD_PARTY_DEPS libuv)
