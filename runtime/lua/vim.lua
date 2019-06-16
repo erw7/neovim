4329c52ce9b057df552234bed677cde66d0c68ee
erw7 <erw7.github@gmail.com>
2019-06-16 17:36:36 +0900

    Add vim.type() function

--- runtime/lua/vim.lua
+++ runtime/lua/vim.lua
@@ -2,5 +2,13 @@ local buffer = require('vim/buffer')
 
 vim.eval = vim.api.nvim_eval
 vim.buffer = buffer
+vim.type = function(obj)
+  local ok, name = pcall(function() return obj.__vim_type end)
+  if ok then
+    return name
+  else
+    return type(obj)
+  end
+end
 
 return vim
