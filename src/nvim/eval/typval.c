f7dd99f6fe0f97066efe61c49c465101eb2a8eb0
erw7 <erw7.github@gmail.com>
2021-04-04 12:59:57 +0900

    Fix problem that callback is not called
    Fix a problem that added callback is not called when dictwatcheradd() is
    called in callback.

--- src/nvim/eval/typval.c
+++ src/nvim/eval/typval.c
@@ -1314,7 +1314,7 @@ void tv_dict_watcher_notify(dict_T *const dict, const char *const key,
   bool any_needs_free = false;
   dict->dv_refcount++;
   QUEUE *w;
-  QUEUE_FOREACH(w, &dict->watchers, {
+  for(w = (&dict->watchers)->next; w != &dict->watchers; w = w->next) {
     DictWatcher *watcher = tv_dict_watcher_node_data(w);
     if (!watcher->busy && tv_dict_watcher_matches(watcher, key)) {
       rettv = TV_INITIAL_VALUE;
@@ -1326,7 +1326,7 @@ void tv_dict_watcher_notify(dict_T *const dict, const char *const key,
         any_needs_free = true;
       }
     }
-  })
+  }
   if (any_needs_free) {
     QUEUE_FOREACH(w, &dict->watchers, {
       DictWatcher *watcher = tv_dict_watcher_node_data(w);
